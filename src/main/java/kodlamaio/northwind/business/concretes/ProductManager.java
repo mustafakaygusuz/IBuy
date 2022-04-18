b44cea21141699443fbaa5775da8bb2f3fea22a6
Mustafa <mustafa@Mustafa-MacBook-Air.local>
2022-04-18 23:47:31 +0300

    first commit

---
+++ src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,37 @@
+package kodlamaio.northwind.business.concretes;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccesResult;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductManager implements ProductService {
+
+    private ProductDao productDao;   //ProductDao injection
+
+    @Autowired     // productDao instance olacak sınıfı üretip veriyor - normalde interface'ler newlenemez
+    public ProductManager(ProductDao productDao) {
+        super();
+        this.productDao = productDao;
+    }
+
+
+    @Override
+    public DataResult<List<Product>> getAll() {
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data listelendi");
+    }
+
+    @Override
+    public Result add(Product product) {
+        this.productDao.save(product);
+        return new SuccesResult("Ürün eklendi");
+    }
+}
